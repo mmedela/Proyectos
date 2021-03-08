f232387b7d7c66d8d7193d817e5a15de38b3b89f
mmedela <65471451+mmedela@users.noreply.github.com>
2021-03-08 20:16:08 -0300

    Add files via upload

--- Cola/cola.c
+++ Cola/cola.c
@@ -1,1 +1,108 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+
+typedef struct nodo{
+
+    void* dato;
+    struct nodo* siguiente;
+
+}nodo_t;
+
+typedef struct cola{
+
+    nodo_t* nodo_primero;
+    nodo_t* nodo_ultimo;
+
+}cola_t;
+
+cola_t* cola_crear(void){
+
+    cola_t* cola = malloc(sizeof(cola_t));
+
+    if (cola != NULL){
+
+        cola->nodo_primero = NULL;
+        cola->nodo_ultimo = NULL;
+    }
+    return cola;
+
+}
+
+bool cola_esta_vacia(const cola_t *cola){
+
+    return ((cola->nodo_primero == NULL)&&(cola->nodo_ultimo == NULL));
+}
+
+bool cola_encolar(cola_t *cola, void* valor){
+
+    nodo_t* nodo = malloc(sizeof(nodo_t));
+    if (!nodo) return false;
+    nodo->dato = valor;
+    nodo->siguiente = NULL;
+    if (cola_esta_vacia(cola)) cola->nodo_primero = nodo;
+
+    else cola->nodo_ultimo->siguiente = nodo;
+    cola->nodo_ultimo = nodo;
+
+    return true;
+}
+
+void* cola_ver_primero(const cola_t *cola){
+
+    if (cola_esta_vacia(cola)){
+
+        return NULL;
+    }
+    return cola->nodo_primero->dato;
+
+}
+
+void* cola_desencolar(cola_t *cola){
+
+    void* respuesta = NULL;
+
+
+    if (cola_esta_vacia(cola)){
+
+        return respuesta;
+    }
+    nodo_t* aux = cola->nodo_primero;
+
+    if (cola->nodo_primero->siguiente == NULL) cola->nodo_ultimo = NULL;
+
+    cola->nodo_primero = cola->nodo_primero->siguiente;
+
+    respuesta = aux->dato;
+
+    free(aux);
+/*
+    if (cola_esta_vacia(cola)){
+
+        cola->nodo_ultimo = NULL;
+    }
+*/
+    return respuesta;
+
+}
+
+
+
+void cola_destruir(cola_t *cola, void (*destruir_dato)(void*)){
+
+    void* dato_leido = NULL;
+    bool cola_vacia = cola_esta_vacia(cola);
+
+    while (!cola_vacia){
+
+        dato_leido = cola_desencolar(cola);
+
+        if (destruir_dato != NULL){
+
+            destruir_dato(dato_leido);
+        }
+        cola_vacia = cola_esta_vacia(cola);
+    }
+    free(cola);
+}
---
+++ Cola/pruebas_alumno.c
@@ -0,0 +1,214 @@
+#include "cola.h"
+#include "testing.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+void evento(void* dato){
+
+    free(dato);
+
+}
+
+void pruebas_cola_vacia(){
+
+    void* var3 = NULL;
+
+    void* dato_leido = NULL;
+
+    cola_t* cola = cola_crear();
+
+    print_test("la cola fue creada", cola!=NULL);
+
+    var3 = cola_ver_primero(cola);
+
+    print_test("lee el priemr elemento de la cola vacia", var3 == NULL);
+
+    dato_leido = cola_desencolar(cola);
+
+    print_test("intenta desencolar cola vacia", dato_leido == NULL);
+
+    print_test("la cola esta vacia", cola_esta_vacia(cola));
+
+    cola_destruir(cola, NULL);
+}
+
+void pruebas_uso_simple(){
+
+    int i = 0;
+
+    int* dato = NULL;
+
+    void* dato_leido = NULL;
+
+    dato = malloc(sizeof(int));
+
+    cola_t* cola = cola_crear();
+
+
+
+    if (!cola) return;
+
+    *dato = 3;
+
+    print_test("la cola fue encolada", cola_encolar(cola, dato));
+
+    dato_leido = cola_ver_primero(cola);
+
+    print_test("se leyo el primero", dato_leido == dato);
+
+    dato_leido = cola_desencolar(cola);
+
+   print_test("la cola se desencolo", dato == dato_leido);
+    free(dato);
+
+    bool todo_ok = true;
+   while (i<10 && todo_ok){
+
+        dato = malloc(sizeof(int));
+        *dato = i;
+        todo_ok = cola_encolar(cola, dato);
+        i++;
+    }
+    print_test("se encolaron", todo_ok);
+    cola_destruir(cola, evento);
+}
+
+void pruebas_volumen(){
+
+    bool respuesta = true;
+    int i = 0;
+    void* dato_leido;
+    cola_t* cola = cola_crear();
+    if(!cola) return;
+    int* dato;
+    while (i<1000 && respuesta){
+
+        dato = malloc(sizeof(int));
+        *dato = i;
+        respuesta = cola_encolar(cola, dato);
+        i++;
+    }
+
+    print_test("se encolaron 1000 elementos",respuesta);
+
+    i=0;
+
+    while (i<1000 && respuesta){
+
+        dato_leido = cola_desencolar(cola);
+
+
+        dato = (int*) dato_leido;
+        if (*dato != i){
+
+            respuesta = false;
+        }
+        evento(dato_leido);
+        i++;
+    }
+
+    print_test("se desencolaron 1000 elementos", respuesta);
+
+    print_test("la cola quedo vacia", cola_esta_vacia(cola));
+    i = 0;
+    while (i<5000 && respuesta){
+
+        dato = malloc(sizeof(int));
+        *dato = i;
+        respuesta = cola_encolar(cola, dato);
+        i++;
+    }
+
+    print_test("se encolaron 5000 elementos",respuesta);
+
+    i=0;
+
+
+    while (i<5000 && respuesta){
+
+        dato_leido = cola_desencolar(cola);
+
+
+        dato = (int*) dato_leido;
+        if (*dato != i){
+
+            respuesta = false;
+        }
+        evento(dato_leido);
+        i++;
+    }
+
+    print_test("se desencolaron 5000 elementos", respuesta);
+
+    print_test("la cola quedo vacia", cola_esta_vacia(cola));
+    i = 0;
+    while (i<10000 && respuesta){
+
+        dato = malloc(sizeof(int));
+        *dato = i;
+        respuesta = cola_encolar(cola, dato);
+        i++;
+    }
+
+    print_test("se encolaron 10000 elementos",respuesta);
+
+    i=0;
+
+    while (i<10000 && respuesta){
+
+        dato_leido = cola_desencolar(cola);
+
+
+        dato = (int*) dato_leido;
+        if (*dato != i){
+
+            respuesta = false;
+        }
+        evento(dato_leido);
+        i++;
+    }
+
+    print_test("se desencolaron 10000 elementos", respuesta);
+
+    print_test("la cola quedo vacia", cola_esta_vacia(cola));
+
+    i = 0;
+    while (i<20000 && respuesta){
+
+        dato = malloc(sizeof(int));
+        *dato = i;
+        respuesta = cola_encolar(cola, dato);
+        i++;
+    }
+
+    print_test("se encolaron 20000 elementos",respuesta);
+
+    i=0;
+
+    while (i<20000 && respuesta){
+
+        dato_leido = cola_desencolar(cola);
+
+
+        dato = (int*) dato_leido;
+        if (*dato != i){
+
+            respuesta = false;
+        }
+        evento(dato_leido);
+        i++;
+    }
+
+    print_test("se desencolaron 20000 elementos", respuesta);
+
+    print_test("la cola quedo vacia", cola_esta_vacia(cola));
+
+    cola_destruir(cola, NULL);
+}
+
+void pruebas_cola_alumno(){
+
+    pruebas_cola_vacia();
+    pruebas_uso_simple();
+    pruebas_volumen();
+}
