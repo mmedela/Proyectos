4ba2a7443744517240217472087bdb5920024637
mmedela <65471451+mmedela@users.noreply.github.com>
2021-03-08 20:25:15 -0300

    Add files via upload

--- Pila/pila.c
+++ Pila/pila.c
@@ -1,1 +1,142 @@
+#include "pila.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#define TAM_PILA_INICIAL  10
+#define REDIMENSION        2
 
+/* Definición del struct pila proporcionado por la cátedra.
+ */
+struct pila {
+    void** datos;
+    size_t cantidad;  // Cantidad de elementos almacenados.
+    size_t capacidad;  // Capacidad del arreglo 'datos'.
+};
+
+void** pila_redimensionar(pila_t* pila, size_t capacidad_nuevo){
+    void** auxiliar = pila->datos;
+        pila->capacidad = capacidad_nuevo;
+        auxiliar = realloc(pila->datos,sizeof(void*) * pila->capacidad);
+    return auxiliar;
+}
+
+/* *****************************************************************
+ *                    PRIMITIVAS DE LA PILA
+ * *****************************************************************/
+pila_t* pila_crear(void)
+{
+    pila_t* p = malloc(sizeof(pila_t));
+    if (p != NULL){
+        p->cantidad = 0;
+        p->capacidad = TAM_PILA_INICIAL;
+        p->datos = malloc(sizeof(void*) * p->capacidad);
+        while (p->datos == NULL){
+            p->datos = malloc(sizeof(void*) * p->capacidad);
+        }
+    }
+    return p;
+}
+
+bool pila_esta_vacia(const pila_t *pila){
+    return ((pila->cantidad) == 0);
+}
+
+void* pila_desapilar(pila_t *pila){
+    void* resul = NULL;
+    void** aux = NULL;
+    if (pila->cantidad == 0) {
+        return NULL;
+    }
+    pila->cantidad--;
+    resul = pila->datos[pila->cantidad];
+    if ((pila->cantidad == (pila->capacidad)/REDIMENSION)&&(pila->capacidad > TAM_PILA_INICIAL)){
+        aux = pila_redimensionar(pila, (pila->capacidad)/REDIMENSION);
+        pila->datos = aux;
+    }
+    return resul;
+}
+void* pila_ver_tope(const pila_t *pila){
+    void* resul = NULL;
+    if (pila->cantidad != 0){
+        resul = pila->datos[pila->cantidad - 1];
+    }
+    return resul;
+}
+
+bool pila_apilar(pila_t *pila, void* valor){
+    if(pila->cantidad == pila->capacidad){
+        pila->datos = pila_redimensionar(pila, (pila->capacidad)*REDIMENSION);
+    }
+    if (pila->datos == NULL){
+        return false;
+    }
+    pila->datos[pila->cantidad] = valor;
+    pila->cantidad++;
+    return true;
+}
+
+void pila_destruir(pila_t *pila){
+    free(pila->datos);
+    free(pila);
+}
+// ...
+void* transformar(void* dato){
+
+    int* a = malloc(sizeof(int));
+    *a = *(int*)dato;
+    *a = *a + 1;
+    return (void*)a;
+}
+pila_t* transformar_pila(pila_t* original, void* (*transformar)(void*)){
+
+    if(!original) return NULL;
+
+    pila_t* pila_trans = NULL;
+    bool todo_ok = false;
+    if(pila_esta_vacia(original)){
+
+        pila_trans = pila_crear();
+        if(!pila_trans)return NULL;
+        return pila_trans;
+    }
+    void* dato = pila_desapilar(original);
+
+    pila_trans = transformar_pila(original, transformar);
+
+    if(pila_trans){
+
+        todo_ok = pila_apilar(pila_trans, transformar(dato));
+
+    }
+    pila_apilar(original, dato);
+    if(!todo_ok){
+
+        while(!pila_esta_vacia(pila_trans)){
+
+            free(pila_desapilar(pila_trans));
+        }
+        pila_destruir(pila_trans);
+        return NULL;
+    }
+    return pila_trans;
+}
+    int main (){
+     pila_t* prueba = pila_crear();
+    for(int i = 0; i < 6; i++){
+
+        int* a = malloc(sizeof(int));
+        *a = i;
+        pila_apilar(prueba, (void*)a);
+    }
+    pila_t* aux = transformar_pila(prueba, transformar);
+    for(int i = 0; i < 6; i++){
+
+        printf("%i ", *(int*)pila_desapilar(aux));
+    }
+    printf("\n");
+    for(int i = 0; i < 6; i++){
+
+        printf("%i ", *(int*)pila_desapilar(prueba));
+    }
+    return 0;
+    }
---
+++ Pila/pruebas_alumno_pila.c
@@ -0,0 +1,77 @@
+#include "pila.h"
+#include "testing.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+
+/* ******************************************************************
+ *                   PRUEBAS UNITARIAS ALUMNO
+ * *****************************************************************/
+
+void pruebas_pila_vacia(){
+    pila_t* pila = NULL;
+    print_test("Puntero inicializado a NULL", pila == NULL);
+    pila = pila_crear();
+    print_test("Puntero a pila inicializado", pila != NULL);
+    print_test("La pila esta vacia (pila_esta_vacia)", pila_esta_vacia(pila));
+    print_test("La pila esta vacia (pila_ver_tope)", !pila_ver_tope(pila));
+    print_test("La pila esta vacia (pila_desapilar)", !pila_desapilar(pila));
+    print_test("la pila permite NULLs", pila_apilar(pila, NULL));
+    pila_destruir(pila);
+}
+void pruebas_pila_volumen(){
+    pila_t* pila = NULL;
+    print_test("Puntero inicializado a NULL", pila == NULL);
+    pila = pila_crear();
+    bool todo_ok;
+    int vector_volumen [10000];
+    for(int i = 0; i < 10000; i++){
+        vector_volumen[i] = i;
+        todo_ok = pila_apilar(pila, &vector_volumen[i]);
+        if (!todo_ok){
+            break;
+        }
+    }
+    print_test("se apilaron 10.000 elementos", todo_ok);
+    print_test("La pila no esta vacia", !pila_esta_vacia(pila));
+    todo_ok = true;
+    for(int i = 10000 -1; i >= 0; i--){
+        if (*(int*)pila_desapilar(pila) != i){
+            todo_ok = false;
+            break;
+        }
+    }
+    print_test ("se desapilaron 10.000 elementos", todo_ok);
+    print_test("La pila quedo vacia", pila_esta_vacia(pila));
+    pila_destruir(pila);
+}
+
+void pruebas_pila_uso_basico(){
+    pila_t* pila = NULL;
+    print_test("Puntero inicializado a NULL", pila == NULL);
+    pila = pila_crear();
+    int vector_nums[12];
+    for (int i = 0; i < 12; i++) {
+        vector_nums[i] = i;
+        print_test("Apilo el elemento i", pila_apilar(pila, &vector_nums[i]));
+        print_test("Leo el elemento i", (int*)pila_ver_tope(pila) == &vector_nums[i]);
+    }
+     print_test("La pila no esta vacia", !pila_esta_vacia(pila));
+    int *a;
+    for(int i = 11; i>=0; i--){
+        a = (int*)pila_desapilar(pila) ;
+       if(*a!=vector_nums[i])a = NULL;
+        print_test("Se desapilo la pila(pila_desapilar)",a!=NULL);
+    }
+    pila_destruir(pila);
+}
+
+
+void pruebas_pila_alumno() {
+
+    pruebas_pila_uso_basico();
+   /* pruebas_pila_volumen();
+    pila_t* pila_vacia = NULL;
+    pila_vacia = pila_crear();
+    pila_destruir(pila_vacia);*/
+}
