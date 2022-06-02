68de97f261f9c04013a1314bdc971c2fa33afb4a
mmedela <mmedela@fi.uba.ar>
2022-06-02 09:47:40 -0300

    feat: draw method

--- side scroll game 1 javaScript/classes/sprite.js
+++ side scroll game 1 javaScript/classes/sprite.js
@@ -2,6 +2,11 @@ class Sprite{
     constructor(position){
         this.position = position;
     }
+
+    draw(canvasContext){
+        canvasContext.fillStyle = 'red';
+        canvasContext.fillRect(this.position.x, this.position.y, 50, 150);
+    }
 }
 
 export default Sprite;
