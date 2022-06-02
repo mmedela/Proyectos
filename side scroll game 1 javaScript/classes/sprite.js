e82ec536537a8e7d0f827d43083775bd5aa266f8
mmedela <mmedela@fi.uba.ar>
2022-06-02 10:19:42 -0300

    feat: falling animation

--- side scroll game 1 javaScript/classes/sprite.js
+++ side scroll game 1 javaScript/classes/sprite.js
@@ -1,11 +1,24 @@
+const gravity = 0.2;
 class Sprite{
-    constructor(position){
+    constructor({position, velocity}){
         this.position = position;
+        this.velocity = velocity;
+        this.height = 150;
     }
 
     draw(canvasContext){
         canvasContext.fillStyle = 'red';
-        canvasContext.fillRect(this.position.x, this.position.y, 50, 150);
+        canvasContext.fillRect(this.position.x, this.position.y, 50, this.height);
+    }
+
+    update(canvasContext, height){
+        this.draw(canvasContext);
+        this.position.y += this.velocity.y;
+        if(this.position.y + this.height + this.velocity.y >= height){
+            this.velocity.y = 0;
+        }else{
+            this.velocity.y += gravity;
+        }
     }
 }
 
--- side scroll game 1 javaScript/index.js
+++ side scroll game 1 javaScript/index.js
@@ -3,12 +3,41 @@ import Sprite from "./classes/sprite.js";
 const canvas = document.querySelector('canvas');
 const canvasContext = canvas.getContext('2d');
 
+const gravity = 0.2;
+
 canvas.width = 1024;
 canvas.height = 576;
 
 canvasContext.fillRect(0,0, canvas.width, canvas.height);
 
-const player = new Sprite({
-    x:0,
-    y:0
+const player1 = new Sprite({
+    position:{
+        x:0,
+        y:0
+    },
+    velocity:{
+        x:0,
+        y:0
+    }
 });
+
+const player2 = new Sprite({
+    position:{
+        x:400,
+        y:100
+    },
+    velocity:{
+        x:0,
+        y:0
+    }
+});
+
+function animate(){
+    window.requestAnimationFrame(animate);
+    canvasContext.fillStyle = 'black';
+    canvasContext.fillRect(0,0,canvas.width, canvas.height);
+    player1.update(canvasContext, canvas.height);
+    player2.update(canvasContext, canvas.height);
+}
+
+animate();
