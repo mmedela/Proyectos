ec829d9b5f3e89cfe982bf056d82e8e2bd2aebd6
mmedela <mmedela@fi.uba.ar>
2022-06-28 15:46:30 -0300

    feat: navbar

--- web_store/src/App.tsx
+++ web_store/src/App.tsx
@@ -1,6 +1,7 @@
 
 import { Container } from "react-bootstrap"
 import {Routes, Route} from "react-router-dom"
+import { Navbar } from "./components/navbar/Navbar"
 import { About } from "./pages/about/About"
 import { Home } from "./pages/home/Home"
 import { Store } from "./pages/store/Store"
@@ -8,13 +9,16 @@ import { Store } from "./pages/store/Store"
 function App() {
  
   return (
-    <Container className="mb-4">
-      <Routes>
-        <Route path="/" element={<Home/>}/> 
-        <Route path="/store" element={<Store/>}/> 
-        <Route path="/about" element={<About/>}/> 
-      </Routes>
-    </Container>
+    <>
+      <Navbar/>
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />}/> 
+          <Route path="/store" element={<Store />}/> 
+          <Route path="/about" element={<About />}/> 
+        </Routes>
+      </Container>
+    </>
   )
 }
 
