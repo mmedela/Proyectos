e36b95f4739d4e8baa8918a990a4df878e3c3c4e
mmedela <mmedela@fi.uba.ar>
2022-06-28 13:54:12 -0300

    feat: routing

--- web_store/src/App.tsx
+++ web_store/src/App.tsx
@@ -1,9 +1,20 @@
 
+import { Container } from "react-bootstrap"
+import {Routes, Route} from "react-router-dom"
+import { About } from "./pages/about/About"
+import { Home } from "./pages/home/Home"
+import { Store } from "./pages/store/Store"
 
 function App() {
  
   return (
-    <h1>Hola</h1>
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home/>}/> 
+        <Route path="/store" element={<Store/>}/> 
+        <Route path="/about" element={<About/>}/> 
+      </Routes>
+    </Container>
   )
 }
 
